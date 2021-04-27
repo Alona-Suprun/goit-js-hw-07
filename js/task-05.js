f5db3d674527b15f1cecceb4b4c88db5824fa08e
Alona-Suprun <a.alonka.l@gmail.com>
2021-04-27 15:02:58 +0300

    task-05-corrected

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,11 @@
 const nameInputEl = document.querySelector('#name-input');
 const nameOutputEl = document.querySelector('#name-output');
 
-const makeName = (name) => {
-    if (nameInputEl.value  === '') {
-        return nameOutputEl.textContent = 'незнакомец';
-    }
-    { nameOutputEl.textContent = name.currentTarget.value ; }
-}
+const makeName = name => {
+  if (nameInputEl.value === '') {
+    return (nameOutputEl.textContent = 'незнакомец');
+  }
+  nameOutputEl.textContent = name.currentTarget.value;
+};
 
 nameInputEl.addEventListener('input', makeName);
