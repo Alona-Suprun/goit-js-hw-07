dd1481c09b648fc5f548d30c67201085f68e69a6
Alona-Suprun <a.alonka.l@gmail.com>
2021-04-27 15:17:06 +0300

    task-08-corrected

--- js/task-08.js
+++ js/task-08.js
@@ -5,31 +5,31 @@ const buttonsEl = document.querySelectorAll('button');
 let boxAmount = 0;
 
 const randomNumber = () => Math.floor(Math.random() * 256);
-const boxesColor = () => `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+const boxesColor = () =>
+  `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
 
-const inputChange = () => boxAmount = inputEl.value;
-  
+const inputChange = () => (boxAmount = inputEl.value);
+
 inputEl.addEventListener('change', inputChange);
 
-const createBoxes = (amount) => {
-    const size = 30;
-    let boxesArray = [];
-    for (let i = 0; i < amount; i += 1) {
-        const box = document.createElement('div')
-       
-        box.style.backgroundColor = boxesColor;
-        box.style.width = i * 10 + size;
-        box.style.height = i * 10 + size;
-    
-         boxesArray.push(box);
-        
-    }
-    boxesEl.append(...boxesArray);
-}
+const createBoxes = amount => {
+  const size = 30;
+  let boxesArray = [];
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement('div');
 
-const buttonRenderClick = () => createBoxes(boxAmount.value);
+    box.style.backgroundColor = boxesColor();
+    box.style.width = i * 10 + size + 'px';
+    box.style.height = i * 10 + size + 'px';
 
-const buttonDestroyClick = () => boxesEl.innerHTML = '';
+    boxesArray.push(box);
+  }
+  boxesEl.append(...boxesArray);
+};
+
+const buttonRenderClick = () => createBoxes(boxAmount);
+
+const buttonDestroyClick = () => (boxesEl.innerHTML = '');
 
 buttonsEl[0].addEventListener('click', buttonRenderClick);
 buttonsEl[1].addEventListener('click', buttonDestroyClick);
